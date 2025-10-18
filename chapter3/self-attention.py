4ec05f0e4ce9e01403ef074303b6d543f68f5de5
aadriien <adriienlynch@gmail.com>
2025-10-17 21:45:42 -0400

    Completed exercise 3.1 after introducing version 2 with improved weight initialization

--- chapter3/self-attention.py
+++ chapter3/self-attention.py
@@ -27,8 +27,46 @@ class SelfAttention_v1(nn.Module):
         return context_vec
 
 
+class SelfAttention_v2(nn.Module):
+    def __init__(self, d_in, d_out, qkv_bias=False):
+        super().__init__()
+        self.W_query = nn.Linear(d_in, d_out, bias=qkv_bias)
+        self.W_key   = nn.Linear(d_in, d_out, bias=qkv_bias)
+        self.W_value = nn.Linear(d_in, d_out, bias=qkv_bias)
 
-def test_self_attention():
+    def forward(self, x):
+        keys = self.W_key(x)
+        queries = self.W_query(x)
+        values = self.W_value(x)
+        
+        attn_scores = queries @ keys.T
+        attn_weights = torch.softmax(
+            attn_scores / keys.shape[-1]**0.5, dim=-1
+        )
+
+        context_vec = attn_weights @ values
+        return context_vec
+
+
+
+def test_self_attention_v1(inputs, d_in, d_out):
+    torch.manual_seed(123)
+    sa_v1 = SelfAttention_v1(d_in, d_out)
+    print(sa_v1(inputs))
+
+
+def test_self_attention_v2(inputs, d_in, d_out):
+    torch.manual_seed(789)
+    sa_v2 = SelfAttention_v2(d_in, d_out)
+    print(sa_v2(inputs))
+
+
+
+################################################################################
+##  MAIN    
+################################################################################
+
+if __name__ == "__main__":
     inputs = torch.tensor(
         [[0.43, 0.15, 0.89], # Your     (x^1)
         [0.55, 0.87, 0.66], # journey  (x^2)
@@ -40,20 +78,10 @@ def test_self_attention():
 
     d_in = inputs.shape[1] # the input embedding size, d=3
     d_out = 2 # the output embedding size, d=2
-    
 
-    torch.manual_seed(123)
-    sa_v1 = SelfAttention_v1(d_in, d_out)
-    print(sa_v1(inputs))
-
-
-
-################################################################################
-##  MAIN    
-################################################################################
-
-if __name__ == "__main__":
     print("\nLoading outputs for self-attention:\n\n")
-    test_self_attention()
+
+    test_self_attention_v1(inputs, d_in, d_out)
+    test_self_attention_v2(inputs, d_in, d_out)
 
 
